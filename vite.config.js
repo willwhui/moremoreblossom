86ca20c87370c85489831d58d9e42e11d9c46c11
3an <3an@sample.com>
2026-04-21 22:09:59 +0900

    feat: add vite-plugin-qrcode to generate network access QR codes during development

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
+import { qrcode } from 'vite-plugin-qrcode';
 
 export default defineConfig({
+  plugins: [
+    qrcode()
+  ],
   server: {
     host: '0.0.0.0', // allow testing from mobile devices on local network
     port: 5174
